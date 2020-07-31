53bf248d60505daa1ed8e81e5272873443bbceb5
egorsmth <mattspring0@gmail.com>
2020-07-31 23:16:21 +0300

    postgre connection tested

--- src/main/java/MainInitializer.java
+++ src/main/java/MainInitializer.java
@@ -4,7 +4,10 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.io.File;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainInitializer implements ServletContextListener {
 
@@ -18,10 +21,45 @@ public class MainInitializer implements ServletContextListener {
         String fullPath = context.getRealPath("") + File.separator + log4jConfigFile;
 
         PropertyConfigurator.configure(fullPath);
+
+        connectDb();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
 
+    }
+
+    private void connectDb()
+    {
+        Connection c = null;
+        String s = "SELECT name FROM public.users";
+
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/testdb",
+                    "admin", "1234");
+            System.out.println("Database Connected ..");
+
+            try (
+                    Statement stmt = c.createStatement();
+                    ResultSet r = stmt.executeQuery(s);)
+            {
+
+                while (r.next()) {
+                    System.out.println(r.getString(1));
+                }
+            }
+           c.close();
+
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+
+
+        System.out.println("Table Created successfully");
     }
 }
